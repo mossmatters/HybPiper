804deb5c9282e53b2e940525c13702af70c009af
Matt Johnson <mjohnson@fabronia.chicagobotanic.org>
2016-02-18 11:20:47 -0600

    added paralog_investigator.py

---
+++ paralog_investigator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+#This script will extract exonerate results for all competing full-length paralogs
+# and deposit them in a new paralog directory within the results directory.
+# Paralogs can then be collected using retrieve_sequences.py in order to align
+# and build gene family trees.
+
+
