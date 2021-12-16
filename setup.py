9b2c732ba2b139bec81f79ee7d97845c9b8c36a3
chrisjackson <chris.jackson@rbg.vic.gov.au>
2021-12-16 21:08:13 +1100

    edit setup for flat install as scripts in bin

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ hybpiper_description = 'Recovery of target gene sequences from bait-capture data
 hybpiper_url = 'https://github.com/chrisjackson-pellicle/HybPiper.git'
 hybpiper_entry_points = {'console_scripts': ['reads_first = hybpiper.reads_first:main',
                                              'retrieve_sequences = hybpiper.retrieve_sequences:main',
-                                             'get_seq_lengths = hybpiper.retrieve_sequences:main',
+                                             'get_seq_lengths = hybpiper.get_seq_lengths:main',
                                              'paralog_retriever = hybpiper.paralog_retriever:main']}
 
 setuptools.setup(name='hybpiper',
