c450434071056f69df26762df719189d726484db
chrisjackson <chris.jackson@rbg.vic.gov.au>
2022-01-12 21:03:09 +1100

    Adding progressbar2 to distribute_reads scripts

--- hybpiper/reads_first.py
+++ hybpiper/reads_first.py
@@ -1299,9 +1299,10 @@ def gene_recovery_heatmap(args):
         print(f'{result.stdout}')
 
     except subprocess.CalledProcessError as exc:
-        print(f'heatmap_command FAILED. Output is: {exc}')
-        print(f'heatmap_command stdout is: {exc.stdout}')
-        print(f'heatmap_command stderr is: {exc.stderr}')
+        print(f'{result.stdout}')
+        # print(f'heatmap_command FAILED. Output is: {exc}')
+        # print(f'heatmap_command stdout is: {exc.stdout}')
+        # print(f'heatmap_command stderr is: {exc.stderr}')
 
 
 def add_assemble_parser(subparsers):
