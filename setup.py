01e6bff95503fdd35df8734bce4b6fbc971ad3d1
chrisjackson <chris.jackson@rbg.vic.gov.au>
2023-07-18 16:04:29 +1000

    import version from version.py module

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@
 
 import setuptools
 
+# Read version number into a dictionary
+version = {}
+with open('hybpiper/version.py') as fp:
+    exec(fp.read(), version)
+
+
 hybpiper_scripts = ['hybpiper']
 hybpiper_description = 'Recovery of target gene sequences from bait-capture data'
 hybpiper_url = 'https://github.com/mossmatters/HybPiper'
 hybpiper_entry_points = {'console_scripts': ['hybpiper = hybpiper.assemble:main']}
 
 setuptools.setup(name='hybpiper',
-                 version='2.1.5',
+                 version=version['__version__'],
                  packages=setuptools.find_packages(),
                  author='Chris Jackson, Matt Johnson',
                  author_email='chris.jackson@rbg.vic.gov.au',
